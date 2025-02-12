e47666c934a3da327bb7621eaf7a28421a81f9cd
cheboi <moscheb96@gmail.com>
2025-02-12 10:36:32 +0300

    feat(shipingOrder, updateshipping, removeshippin)

--- controllers/shippingController.js
+++ controllers/shippingController.js
@@ -39,3 +39,53 @@ export const getShippingById = async (req, res) => {
     res.status(500).json({ error: "Server error while feching record" });
   }
 };
+
+export const getShippingByOrderId = async (req, res) => {
+  try {
+    const { order_id } = req.params;
+    const shippingRecords = await shippingModel.getShippingByOrderId(order_id);
+    res.status(200).json(shippingRecords);
+  } catch (err) {
+    console.error("Error fetching shipping records for order:", err.message);
+    res.status(500).json({
+      error: "Server error while fetching shipping records for order",
+    });
+  }
+};
+
+export const updateShipping = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+    const updatedShipping = await shippingModel.updateShipping(id, updateData);
+    if (!updatedShipping)
+      return res.status(404).json({ error: "Shipping record not found" });
+    res
+      .status(200)
+      .json({
+        message: "Shipping updated successfully",
+        shipping: updatedShipping,
+      });
+  } catch (err) {
+    console.error("Error updating shipping:", err.message);
+    res.status(500).json({ error: "Server error while updating shipping" });
+  }
+};
+
+export const deleteShipping = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedShipping = await shippingModel.deleteShipping(id);
+    if (!deletedShipping)
+      return res.status(404).json({ error: "Shipping record not found" });
+    res
+      .status(200)
+      .json({
+        message: "Shipping deleted successfully",
+        shipping: deletedShipping,
+      });
+  } catch (err) {
+    console.error("Error deleting shipping:", err.message);
+    res.status(500).json({ error: "Server error while deleting shipping" });
+  }
+};
--- routes/shippingRoutes.js
+++ routes/shippingRoutes.js
@@ -1,6 +1,18 @@
 import express from "express";
 import * as shippingController from "../controllers/shippingController.js";
-import verifyToken from "../middleware/verifyToken.js";
+// import verifyToken from '../middleware/verifyToken.js';
+
+const router = express.Router();
+
+// router.use(verifyToken);
 
 router.post("/", shippingController.createShipping);
 router.get("/:id", shippingController.getShippingById);
+
+router.get("/order/:order_id", shippingController.getShippingByOrderId);
+
+router.put("/:id", shippingController.updateShipping);
+
+router.delete("/:id", shippingController.deleteShipping);
+
+export default router;
