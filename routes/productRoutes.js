6f0d6938f967b9b1c11b988f15ac8d7116d613d6
cheboi <moscheb96@gmail.com>
2025-02-28 10:30:45 +0300

    update: authMiddleware, and protect products routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,14 @@
 import express from "express";
 import * as productController from "../controllers/productController.js";
-// import verifyToken from "../middleware/verifyToken.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// router.use(verifyToken);
-
-router.post("/", productController.createProduct);
+router.post("/", authMiddleware, productController.createProduct);
 router.get("/", productController.getAllProducts);
 router.get("/search", productController.searchProducts);
 router.get("/:id", productController.getProductById);
-router.put("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+router.put("/:id", authMiddleware, productController.updateProduct);
+router.delete("/:id", authMiddleware, productController.deleteProduct);
 
 export default router;
